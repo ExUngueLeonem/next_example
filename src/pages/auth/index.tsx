6165cda25b8c7696c1926f350ad32fe8c5b23ba7
Ilya Krukov <ujfox13@gmail.com>
2023-03-14 11:46:40 +0300

    init

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Auth.module.scss';
+
+const Auth = () => {
+    return (
+        <div className={styles.foo}>
+            Auth
+        </div>
+    );
+};
+
+export default Auth;
