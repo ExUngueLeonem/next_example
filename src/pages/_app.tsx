45d6c8e20da4f3170417d4e39b1d3b0765d72a01
Ilya Krukov <ujfox13@gmail.com>
2023-03-15 11:41:06 +0300

    link for styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import '@/styles/reset.css'
+import '@/styles/globals.scss'
 import '@/styles/_mixins.scss'
+import '@/styles/_variables.scss'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
