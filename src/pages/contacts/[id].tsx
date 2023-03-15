d32232a7b0d7124509dc7703affc417f62c31894
Ilya Krukov <ujfox13@gmail.com>
2023-03-15 12:51:32 +0300

    typescript warning fix

--- src/pages/contacts/[id].tsx
+++ src/pages/contacts/[id].tsx
@@ -2,6 +2,7 @@ import ContactInfo from '@/components/ContactInfo';
 import Layout from '@/components/Layout';
 import { IUser } from '@/models/contacts';
 import { GetServerSideProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
 
 const Contact = ({ contact }: { contact: IUser }) => {
     return (
@@ -11,9 +12,12 @@ const Contact = ({ contact }: { contact: IUser }) => {
     );
 }
 
+interface IParams extends ParsedUrlQuery {
+    id: string;
+}
 //getServerSideProps - это сервер сайд рендеринг
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    const { id } = ctx.query
+    const { id } = ctx.params as IParams
 
     try {
         const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
