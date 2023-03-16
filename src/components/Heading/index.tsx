4a5fbb47a7642611c2764de30a2ee2df629daf14
Ilya Krukov <ujfox13@gmail.com>
2023-03-16 15:47:02 +0300

    fix active link

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -2,17 +2,29 @@
 import { FC } from 'react';
 import Link from 'next/link';
 import styles from './Heading.module.scss';
+import { useRouter } from 'next/router';
 
 const Heading: FC = () => {
+    const router = useRouter();
+
+    const navLinks = [
+        { name: "Home", href: "/" },
+        { name: "Posts", href: "/posts" },
+        { name: "Contacts", href: "/contacts" },
+    ]
+
     return (
         <div className={styles.header}>
             <div className={styles.logo}>
                 Logo
+                {router.pathname}
             </div>
             <div className={styles.nav_list}>
-                <Link href={"/"}>Home</Link>
-                <Link href={"/posts"}>Posts</Link>
-                <Link href={"/contacts"}>Contacts</Link>
+                {navLinks.map(item => {
+                    let activeClass
+                    if (router.pathname === item.href) activeClass = styles.active
+                    return <Link className={activeClass} key={item.name} href={item.href}>{item.name}</Link>
+                })}
             </div>
         </div>
     );
