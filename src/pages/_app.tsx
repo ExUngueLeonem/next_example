7a6e4e42e2d2326af215cd9e76be97d13dd6c1cf
Ilya Krukov <ujfox13@gmail.com>
2023-03-20 13:29:39 +0300

    fetcher

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,13 @@ import 'styles/_mixins.scss'
 import 'styles/_variables.scss'
 import type { AppProps } from 'next/app'
 import { SWRConfig } from 'swr'
+import fetchJson from 'lib/fetchJson'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SWRConfig
       value={{
-        fetcher: () => { },
+        fetcher: fetchJson,
         onError: (error: any) => {
           console.error(error)
         }
